13c701248254847c1bc6d25abe7dfb10c8dd929d
Mare94 <Morten.Sandstedt@stud.noroff.no>
2023-02-17 16:33:51 +0100

    added script link

--- js/post.js
+++ js/post.js
@@ -1,9 +1,11 @@
-const postContainer = document.querySelector(".post-container")
+const postContainer = document.querySelector(".post-container");
 
 // API call url
 const url = "https://powerofflower.online/cms-ma1/flower-power/wordpress/wp-json/wp/v2/posts";
 
 //Function calling posts from API
+
+console.log("hello world");
 
 async function getPostData(){
     try{
