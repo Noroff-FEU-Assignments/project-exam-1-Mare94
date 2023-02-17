e7edf2f64b3c997835c4a8e8470baab59c87a460
Mare94 <Morten.Sandstedt@stud.noroff.no>
2023-02-17 16:31:12 +0100

    created async function

--- js/post.js
+++ js/post.js
@@ -2,3 +2,21 @@ const postContainer = document.querySelector(".post-container")
 
 // API call url
 const url = "https://powerofflower.online/cms-ma1/flower-power/wordpress/wp-json/wp/v2/posts";
+
+//Function calling posts from API
+
+async function getPostData(){
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data);
+
+    }
+    catch(error){
+
+    }
+
+};
+
+getPostData();
