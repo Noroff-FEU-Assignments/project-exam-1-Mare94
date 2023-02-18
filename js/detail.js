6c6f4a51ac2ce71aa130c63efcf1a67536a1c95a
Mare94 <Morten.Sandstedt@stud.noroff.no>
2023-02-18 14:14:39 +0100

    added script detail, linked script

---
+++ js/detail.js
@@ -0,0 +1,24 @@
+const detailContainer = document.querySelector(".blog-detail");
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const urlName = params.get("id");
+
+const url = "https://powerofflower.online/cms-ma1/flower-power/wordpress/wp-json/wp/v2/posts";
+
+const detailUrl = url + urlName;
+
+
+async function getPostDetail(){
+    try{
+
+        const response = await fetch(detailUrl);
+        const detail = await response.json();
+
+        console.log(detail);
+
+    }
+    catch(error){
+
+    }
+}
