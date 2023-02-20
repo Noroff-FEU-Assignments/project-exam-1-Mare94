e56cbb4e9ae199d5c67ddaa7331d8b1bf531af15
Mare94 <Morten.Sandstedt@stud.noroff.no>
2023-02-20 21:16:40 +0100

    carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,10 @@
 const caroContainer = document.querySelector(".caro-slide", "current-slide");
+const caroContainerTwo = document.querySelector(".caro-slide2", "current-slide");
+const caroContainerThee = document.querySelector(".caro-slide3", "current-slide");
+const caroContainerFour = document.querySelector(".caro-slide4", "current-slide");
 
 // API call url
-const caroUrl = "https://powerofflower.online/cms-ma1/flower-power/wordpress/wp-json/wp/v2/posts";
+let caroUrl = "https://powerofflower.online/cms-ma1/flower-power/wordpress/wp-json/wp/v2/posts";
 
 //Function calling posts from API
 
@@ -14,24 +17,23 @@ async function getPostData(){
 
         console.log(data);
 
-        for(let i = 0; i < 4; i++){
-
+        for(let i = 0; i < 1; i++){
 
             
 
             caroContainer.innerHTML += `
                                             <div class="caro-slide">
-                                            <a href="detail.html?id=${data[i].id}"> 
-                                                <h3>${data[i].title.rendered} :</h3>
-                                                <h4>${data[i].excerpt.rendered}</h4>
-                                                <h4>${data[i].date}</h4>
+                                            <a href="detail.html?id=${data[0].id}"> 
+                                                <h3>${data[0].title.rendered} :</h3>
+                                                <h4>${data[0].excerpt.rendered}</h4>
+                                                <h4>${data[0].date}</h4>
                                             </a>
-                                            </div>
                                             
                                                                               
                                         `
 
         };
+        
 
     }
     catch(error){
@@ -41,3 +43,38 @@ async function getPostData(){
 };
 
 getPostData();
+
+
+async function getPostDataTwo(){
+    try{
+        const response = await fetch(caroUrl);
+        const data = await response.json();
+
+        console.log(data);
+
+        for(let i = 1; i < 2; i++){
+
+            
+
+            caroContainerTwo.innerHTML += `
+                                            <div class="caro-slide2">
+                                            <a href="detail.html?id=${data[1].id}"> 
+                                                <h3>${data[1].title.rendered} :</h3>
+                                                <h4>${data[1].excerpt.rendered}</h4>
+                                                <h4>${data[1].date}</h4>
+                                            </a>
+                                            
+                                                                              
+                                        `
+
+        };
+        
+
+    }
+    catch(error){
+
+    }
+
+};
+
+getPostDataTwo();
