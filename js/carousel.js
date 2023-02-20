ddc25dea5b361051dcd48faf188fb1378e99fc90
Mare94 <Morten.Sandstedt@stud.noroff.no>
2023-02-20 21:41:10 +0100

    changes

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,6 @@
-const caroContainer = document.querySelector(".caro-slide", "current-slide");
+const caroContainerOne = document.querySelector(".caro-slide", "current-slide");
 const caroContainerTwo = document.querySelector(".caro-slide2", "current-slide");
-const caroContainerThee = document.querySelector(".caro-slide3", "current-slide");
+const caroContainerThree = document.querySelector(".caro-slide3", "current-slide");
 const caroContainerFour = document.querySelector(".caro-slide4", "current-slide");
 
 // API call url
@@ -21,7 +21,7 @@ async function getPostData(){
 
             
 
-            caroContainer.innerHTML += `
+            caroContainerOne.innerHTML += `
                                             <div class="caro-slide">
                                             <a href="detail.html?id=${data[0].id}"> 
                                                 <h3>${data[0].title.rendered} :</h3>
@@ -44,6 +44,7 @@ async function getPostData(){
 
 getPostData();
 
+// Slide 2
 
 async function getPostDataTwo(){
     try{
@@ -78,3 +79,76 @@ async function getPostDataTwo(){
 };
 
 getPostDataTwo();
+
+// Slide 3
+
+async function getPostDataThree(){
+    try{
+        const response = await fetch(caroUrl);
+        const data = await response.json();
+
+        console.log(data);
+
+        for(let i = 2; i < 3; i++){
+
+            
+
+            caroContainerThree.innerHTML += `
+                                            <div class="caro-slide3">
+                                            <a href="detail.html?id=${data[2].id}"> 
+                                                <h3>${data[2].title.rendered} :</h3>
+                                                <h4>${data[2].excerpt.rendered}</h4>
+                                                <h4>${data[2].date}</h4>
+                                            </a>
+                                            
+                                                                              
+                                        `
+
+        };
+        
+
+    }
+    catch(error){
+
+    }
+
+};
+
+getPostDataThree();
+
+
+// Slide 4 
+
+async function getPostDataFour(){
+    try{
+        const response = await fetch(caroUrl);
+        const data = await response.json();
+
+        console.log(data);
+
+        for(let i = 3; i < 4; i++){
+
+            
+
+            caroContainerFour.innerHTML += `
+                                            <div class="caro-slide4">
+                                            <a href="detail.html?id=${data[3].id}"> 
+                                                <h3>${data[3].title.rendered} :</h3>
+                                                <h4>${data[3].excerpt.rendered}</h4>
+                                                <h4>${data[3].date}</h4>
+                                            </a>
+                                            
+                                                                              
+                                        `
+
+        };
+        
+
+    }
+    catch(error){
+
+    }
+
+};
+
+getPostDataFour();
