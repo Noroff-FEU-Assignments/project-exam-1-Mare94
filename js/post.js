a945c2f2924b038783d7b649b2e2b862443b7560
Mare94 <Morten.Sandstedt@stud.noroff.no>
2023-02-21 17:12:02 +0100

    changed posts url

--- js/post.js
+++ js/post.js
@@ -1,7 +1,7 @@
 const postContainer = document.querySelector(".post-container");
 
 // API call url
-const url = "https://powerofflower.online/cms-ma1/flower-power/wordpress/wp-json/wp/v2/posts";
+const url = "https://powerofflower.online/cms-ma1/flower-power/wordpress/wp-json/wp/v2/posts?per_page=20";
 
 //Function calling posts from API
 
