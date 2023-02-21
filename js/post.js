7bbf71c886958723a303692ed5340f680a581a5a
Mare94 <Morten.Sandstedt@stud.noroff.no>
2023-02-21 17:49:26 +0100

    added btn blog html

--- js/post.js
+++ js/post.js
@@ -8,6 +8,7 @@ const url = "https://powerofflower.online/cms-ma1/flower-power/wordpress/wp-json
 
 async function getPostData(){
     try{
+        
         const response = await fetch(url);
         const data = await response.json();
 
